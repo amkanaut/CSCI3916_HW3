fecb41d2d02ecb99c7c480fbb8e0ba4ab2fad8c7
Amka Baasanbat <141882871+amkanaut@users.noreply.github.com>
2026-03-15 14:14:26 -0600

    Update Movies.js

--- models/Movies.js
+++ models/Movies.js
@@ -1,22 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Took out connectDB to centralize it in server.js
-
-// Movie schema
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
-  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+  releaseDate: { 
+    type: Number, 
+    min: [1900, 'Must be greater than 1899'], 
+    max: [2100, 'Must be less than 2100'] 
+  },
   genre: {
     type: String,
     enum: [
       'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
     ],
   },
-  actors: [{
-    actorName: String,
-    characterName: String,
-  }],
+  actors: {
+    type: [{
+      actorName: String,
+      characterName: String,
+    }],
+    validate: {
+      validator: function(v) {
+        return v && v.length >= 3;
+      },
+      message: "Movie must have at least 3 actors"
+    }
+  }
 });
 
 module.exports = mongoose.model('Movie', MovieSchema, 'Movies');
