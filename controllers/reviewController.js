8c03e6dcddd0188dd9a18fb3e568afc49a3b68b1
Amka Baasanbat <141882871+amkanaut@users.noreply.github.com>
2026-04-05 15:35:42 -0600

    Fixed reviewController so that it checks if movie exists

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const { MongoGridFSChunkError } = require('mongodb');
 const Review = require('../models/Reviews');
+const Movie = require('../models/Movies');
 
 // @desc Created new controller for Review methods
 // @route POST api/reviews
@@ -9,6 +10,16 @@ const Review = require('../models/Reviews');
 
 const postReviews = async (req, res) => {
     try {
+        const movie = await Movie.findById(req.body.movieId);
+
+        if (!movie) {
+            return res.status(404).json({
+                message: 'Movie does not exist, cannot create review'
+            });
+        }
+
+
+
         const newReview = new Review({
             movieId: req.body.movieId,
             username: req.user.username,
@@ -17,6 +28,7 @@ const postReviews = async (req, res) => {
     });
         const savedReview = await newReview.save();
         res.status(201).json({ message: 'Review created!' });
+        
     } catch (error) {
         res.status(400).json({ 
             message: 'Failed to create review due to invalid data', 
